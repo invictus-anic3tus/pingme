bee6c4094d8b72a9a22380465a12ffb388de47c9
invictus-anic3tus <anic3tus.inv@gmail.com>
2025-01-13 14:25:18 -0600

    update send-notification.js, index.html and vercel.json

--- api/send-notification.js
+++ api/send-notification.js
@@ -1,19 +1,23 @@
-//import webPush from "web-push";
-const webPush = require('web-push');
-
+const webPush = require("web-push");
 
 const publicKey =
   "BP6LNLZ3mgjKV3t_wO4DZdnd1QwAOWN_VIPoNWRB8-w4T2ATVEMIcuLgkm2D7L0uAAWX0oaMhWoUtQH-TjLqUvU";
-const privateKey = "8lUs0i4BU0M8cVRcHTLDzrhnmMqAd35U5Zq-4UiVsTk";
+const privateKey = "<private_vapid_key>"; // Yes my real key is here
 
 webPush.setVapidDetails("mailto:anic3tus.inv@gmail.com", publicKey, privateKey);
 
-
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    console.log("payload: ", req.body);
+
     const { subscription, title, message, actions, tag } = req.body;
 
     const notificationActions = Array.isArray(actions) ? actions : [];
+
+    if (!subscription || !subscription.endpoint) {
+      console.error("Invalid subscription object:", subscription);
+      return res.status(400).json({ error: "Invalid subscription" });
+    }
 
     // why am i supsosed to have a regustration thing here??
     try {
@@ -21,7 +25,7 @@ export default async function handler(req, res) {
       await webPush.sendNotification(
         subscription,
         JSON.stringify({
-          title: title,
+          title: title || 'Ping title',
           body: message || "Your ping has triggered! (Name not sent)",
           icon: "/images/PingMe.png",
           actions: notificationActions,
